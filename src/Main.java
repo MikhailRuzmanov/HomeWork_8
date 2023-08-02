22d8f9485891f4c819025a87d8995712307eb531
MikhailRuzmanov <mihailruzmanov@mail.ru>
2023-08-03 01:04:11 +0300

    Проверить домашнее задание №8

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
 
     public static void task1 () {
+        System.out.println("Домашнее задание №8");
         System.out.println("");
         System.out.println("============================");
         System.out.println("======= Задача 1 ===========");
